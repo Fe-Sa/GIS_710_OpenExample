cb667cd2b9fc2a2eaa8f839c01803f3652668034
Felipe Sanchez <46250208+Fe-Sa@users.noreply.github.com>
2021-11-22 10:57:16 -0500

    Adding code to plot with matplotlib

--- Rasterio_import.py
+++ Rasterio_import.py
@@ -2,3 +2,10 @@ import rasterio
 
 with rasterio.open("http://fatra.cnr.ncsu.edu/us-iale2017/US_elevation.tif") as dem:
     print(dem.crs)
+    
+dem = rasterio.open("http://fatra.cnr.ncsu.edu/us-iale2017/US_elevation.tif")
+array = dem.read(1)
+from matplotlib import pyplot
+%matplotlib inline
+pyplot.imshow(array, cmap='pink')
+pyplot.show()
